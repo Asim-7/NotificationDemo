1b04b2e8565d4b54b72bd46fb533ea4a76edea7a
Asim-7 <asimpsy11@gmail.com>
2020-03-16 20:35:16 +0500

    Initial commit

---
+++ app/src/main/java/com/shehzad/notificationdemo/NotificationActivity.java
@@ -0,0 +1,19 @@
+package com.shehzad.notificationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        TextView textView = findViewById(R.id.myTxt);
+        String message = getIntent().getStringExtra("message");
+        textView.setText(message);
+    }
+}
